fc4a7df26fc21df5131c12ed3fa8b80f6ede33a8
casonbobo <casonsbobo@gmail.com>
2023-04-21 10:56:42 -0700

    TRAILING WHITE SPACE

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (!parent)
 		return (NULL);
-	
+
 	newNode = parent->left;
 	parent->left = binary_tree_node(parent, value);
 	if (!newNode)
