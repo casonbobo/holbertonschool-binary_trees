7b426c6a44cbbe3ba1ba1671f260c42206fe2b9f
casonbobo <casonsbobo@gmail.com>
2023-04-21 15:20:44 -0700

    peepee poopoo

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,9 +6,10 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->left == NULL && node->right == NULL)
+	if (node == NULL)
+		return (0);
+	if (node->parent == NULL)
 		return (1);
 	else
 		return (0);
-	return (0);
 }
