97de59aeaff26c30ce48ba4782b79900841b4918
casonbobo <casonsbobo@gmail.com>
2023-04-21 11:18:37 -0700

    this is for the right node

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_insert_left - sets a node to the left of the parent
+ *@parent: where did you go?
+ *@value: is very important to me
+ *Return: return me back to my parent
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	if (!parent)
+		return (NULL);
+
+	newNode = parent->right;
+	parent->right = binary_tree_node(parent, value);
+	if (!newNode)
+		return (NULL);
+	parent->right->right = newNode;
+	if (newNode)
+		newNode->parent = parent->right;
+	return (parent->right);
+}
