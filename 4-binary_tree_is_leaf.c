53386742f483b4490f966b0c48d7393f92c334fc
casonbobo <casonsbobo@gmail.com>
2023-04-21 14:33:36 -0700

    this is peepee poopoo

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_is_leaf - is this a leaf
+ *@node: is this a node
+ *Return: return one or zero
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node->left != NULL || node->right != NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
