7d2af8b25c2c0ed6edc8d0790993d60d7d71bfc6
casonbobo <casonsbobo@gmail.com>
2023-04-21 11:31:57 -0700

    changed the naming to the right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 /**
- *binary_tree_insert_left - sets a node to the left of the parent
+ *binary_tree_insert_right - sets a node to the right of the parent
  *@parent: where did you go?
  *@value: is very important to me
  *Return: return me back to my parent
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
