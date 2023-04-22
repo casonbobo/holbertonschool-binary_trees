06b6013ec2c1a3148f76c8499fac66e3a784ddc5
casonbobo <casonsbobo@gmail.com>
2023-04-22 14:41:37 -0700

    this is for height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_height - counts the height of the tree
+ *@tree: is the argumentative tree
+ *Return:right and left
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left = 0, height_right = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		height_left = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		height_right = 1 + binary_tree_height(tree->right);
+	
+	if (height_left > height_right)
+		return (height_left);
+	else
+		return (height_right);
+}
