bfcdc2901e82bebacabb54c0d1a3adef1d629738
casonbobo <casonsbobo@gmail.com>
2023-04-22 13:47:31 -0700

    inorder function

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 /**
- *binary_tree_preorder - is a type of tree traversal
+ *binary_tree_inorder - is a type of tree traversal
  *@tree: the tree we're calling
  *@func:a func call for the tree
  *Return: nothing
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
 
-	func(tree->n);
 	binary_tree_preorder(tree->left, func);
+	func(tree->n);
 	binary_tree_preorder(tree->right, func);
 }
