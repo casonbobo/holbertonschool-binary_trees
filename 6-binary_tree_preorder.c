6d4436dc833b2c7900f953daab446e563280b3fd
casonbobo <casonsbobo@gmail.com>
2023-04-22 13:38:12 -0700

    comments are done

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
- *
+ *binary_tree_preorder - is a type of tree traversal
+ *@tree: the tree we're calling
+ *@func:a func call for the tree
+ *Return: nothing
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
